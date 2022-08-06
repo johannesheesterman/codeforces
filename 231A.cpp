6bcf97fd7b1983e2d067e85ed48e6ff5774a43cf
Johannes Heesterman <johannes@elfsquad.io>
2022-08-06 21:46:45 +0200

    231A

---
+++ 231A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x + y + z > 1) {
+            ++sum;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
