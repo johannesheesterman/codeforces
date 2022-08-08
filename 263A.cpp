9de131082438fa8a356df01fbffdbbb32d445a1b
Johannes Heesterman <johannes@elfsquad.io>
2022-08-08 21:33:48 +0200

    263A

--- 263A.cpp
+++ 263A.cpp
@@ -4,18 +4,12 @@
 using namespace std;
 
 int main() {
-    int row = 0;
-    int col = 0;
-
     for (int j = 0; j < 5; j++) {
         for (int i = 0; i < 5; i++) {
             int num; 
             cin >> num;
             if (num == 1) {
-                row = j + 1;
-                col = i + 1;
-
-                cout << (abs(row - 3) + abs(col - 3)) << endl; 
+                cout << (abs(j - 3) + abs(i - 3)) << endl; 
                 return 0;
             }
         }
